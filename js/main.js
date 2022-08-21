e99f16cd0f9abc3a66a87b263e6ab3a497df3add
Roman Chentsov <Core2-00@mail.ru>
2022-08-22 01:51:15 +0500

    add util functions

---
+++ js/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const getRandomNum = (start, end) => {
+  if (start < 0 || end < 0) {
+    throw new TypeError('Допускаются только положительные числа!');
+  }
+
+  if (isNaN(start) || isNaN(end)) {
+    throw new TypeError('Параметры могут быть только числами!');
+  }
+
+  if (start === end) {
+    throw new TypeError('Выполнение операции невозможно!');
+  }
+
+  if (start > end) {
+    [start, end] = [end, start];
+  }
+
+  start = Math.ceil(start);
+  end = Math.floor(end);
+
+  return Math.floor(Math.random() * (end - start + 1)) + start;
+};
+
+const isLengthLimit = (string, maxLength) => {
+  return string.length <= maxLength;
+};
+
+getRandomNum(1, 5);
+isLengthLimit('hello', 4);
