536ee23c7b9737fe87d3329735102965c8e10f4a
Core2-0 <Core2-00@mail.ru>
2022-08-24 16:05:52 +0500

    add slider effects

--- js/settings-upload-photo.js
+++ js/settings-upload-photo.js
@@ -17,6 +17,7 @@ const imgUploadPreview = document.querySelector('.img-upload__preview > img');
 
 const sliderField = document.querySelector('.effect-level')
 const sliderElement = sliderField.querySelector('.effect-level__slider');
+const sliderValue = sliderField.querySelector('.effect-level__value');
 
 const filterChange = document.querySelectorAll('.effects__radio');
 
@@ -49,31 +50,95 @@ setImageScale();
 
 // SLIDER
 
+const effects = [
+  {
+    filter: 'none',
+  },
+  {
+    filter: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+  },
+  {
+    filter: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+  },
+  {
+    filter: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    suffix: '%',
+  },
+  {
+    filter: 'blur',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    suffix: 'px',
+  },
+  {
+    filter: 'brightness',
+    min: 1,
+    max: 3,
+    step: 0.1,
+  },
+]
+
 noUiSlider.create(sliderElement, {
   range: {
     min: 0,
-    max: 100,
+    max: 1,
   },
-  start: 80,
-  step: 1,
+  start: 1,
+  step: 0.1,
   connect: 'lower',
-})
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+});
 
 // FILTER
 const setFilter = () => {
   sliderField.classList.add('hidden');
 
   for (let i = 0; i < filterChange.length; i++) {
-    filterChange[i].addEventListener('click', () => {
+    filterChange[i].addEventListener('change', () => {
       if (filterChange[i].value !== 'none') {
         sliderField.classList.remove('hidden');
         imgUploadPreview.removeAttribute('class');
         imgUploadPreview.classList.add(`effects__preview--${filterChange[i].value}`);
+
+        sliderElement.noUiSlider.on('update', (values, handle) => {
+          sliderValue.value = values[handle];
+          imgUploadPreview.style.filter = `${effects[i].filter}(${effects[i].suffix ? sliderValue.value + effects[i].suffix : sliderValue.value})`;
+        });
+
+        sliderElement.noUiSlider.updateOptions({
+          range: {
+            min: effects[i].min,
+            max: effects[i].max,
+          },
+          step: effects[i].step,
+        });
+        sliderElement.noUiSlider.set(effects[i].max);
       } else {
         sliderField.classList.add('hidden');
         imgUploadPreview.removeAttribute('class');
+        imgUploadPreview.removeAttribute('style');
       }
-    })
+    });
   }
 }
 
