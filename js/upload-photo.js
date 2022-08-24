d7c2f96b6369282825263182501f06bf44281dd0
Roman Chentsov <Core2-00@mail.ru>
2022-08-24 20:10:29 +0500

    add close modal with esc key

--- js/upload-photo.js
+++ js/upload-photo.js
@@ -1,5 +1,6 @@
 import { setImageScale, imgUploadPreview, scaleControlValue } from './scale-photo.js';
 import { setFilter } from './effects.js';
+import { isEcsEvent } from './utils/utils.js';
 
 const imgUpLoad = document.querySelector('#upload-file');
 const imgUpLoadForm = document.querySelector('.img-upload__overlay');
@@ -10,7 +11,9 @@ imgUpLoad.addEventListener('change', () => {
   imgUpLoadForm.classList.remove('hidden');
   document.querySelector('body').classList.add('modal-open');
   setFilter();
-})
+
+  document.addEventListener('keydown', onCloseUpLoadEsc);
+});
 
 setImageScale();
 
@@ -19,11 +22,23 @@ const resetSettigns = () => {
   imgUploadPreview.style.filter = '';
   imgUploadPreview.style.transform = 'scale(1)';
   scaleControlValue.value = `${100}%`;
+};
+
+const closeUpLoad = () => {
+  imgUpLoadForm.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  imgUpLoad.value = '';
+
+  document.removeEventListener('keydown', onCloseUpLoadEsc);
+};
+
+const onCloseUpLoadEsc = (evt) => {
+  if (isEcsEvent(evt)) {
+    evt.preventDefault();
+    closeUpLoad();
+  }
 }
 
 buttonCancel.addEventListener('click', () => {
-  imgUpLoadForm.classList.add('hidden');
-  document.querySelector('body').classList.remove('modal-open');
-
-  imgUpLoad.value = '';
+  closeUpLoad();
 });
